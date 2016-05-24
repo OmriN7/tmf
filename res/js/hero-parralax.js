3265edc473f0c6876cdf9e2978f6b0f89b9fe605
Omri Gideoni <omrig.n7@gmail.com>
2016-05-24 03:25:53 -0700

    Update

--- res/js/hero-parralax.js
+++ res/js/hero-parralax.js
@@ -1,8 +1,19 @@
 $(window).scroll(function()
 {
+	heroParralax();
+});
+
+ $(document).ready(function() 
+ {
+	heroParralax();
+ });
+
+function heroParralax()
+{
 	var scrollOffset = $(this).scrollTop();
 	
 	$(".hero__header").css("transform", "translateY(" + scrollOffset /5 + "px)");
 	$(".hero--md").css("background-position", "50% " + ((scrollOffset /3)-20) + "px");
 	$(".hero--md").css("filter", "grayscale(" + (scrollOffset/5) + "%)");
-});
+	$(".hero--md").css("-webkit-filter", "grayscale(" + (scrollOffset/5) + "%)");
+}
--- res/js/stripe-parralax.js
+++ res/js/stripe-parralax.js
@@ -1,8 +1,18 @@
 $(window).scroll(function()
 {
+	stripeParralax();
+});
+
+ $(document).ready(function() 
+ {
+	stripeParralax();
+ });
+ 
+function stripeParralax()
+{
 	var scrollOffset = $(this).scrollTop();
 	
 	$(".js-stripe--bg0-parralax").css("background-position", "50% " + (((scrollOffset)-($(".js-stripe--bg0-parralax").offset().top))/10) + "px");
 	$(".js-stripe--bg1-parralax").css("background-position", "50% " + (((scrollOffset)-($(".js-stripe--bg1-parralax").offset().top))/10) + "px");
 	$(".js-stripe--bg2-parralax").css("background-position", "50% " + (((scrollOffset)-($(".js-stripe--bg2-parralax").offset().top))/10) + "px");
-});
+}
